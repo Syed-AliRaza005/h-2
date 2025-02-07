a48a59ee1b0acc88f6009a47a46a293124fba3f8
Syed-AliRaza005 <162878270+Syed-AliRaza005@users.noreply.github.com>
2025-02-07 21:11:22 +0500

    Update page.tsx

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -84,7 +84,7 @@ export default function SlugPage({ params }: { params: { slug: string } }) {
 
         <div className="max-w-6xl mx-auto p-6">
           <h1 className="text-center font-extrabold text-4xl text-gray-800 mb-6">
-            Product Details of "{product.title}"
+            Product Details of {product.title}
           </h1>
 
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
